0aead2b288146ae4660e8fb28d15a50d8f7ef4c9
Darren <guobaoyan93@gmail.com>
2021-07-17 20:31:04 +0800

    搜索旋转排序数组

---
+++ 数组/搜索旋转排序数组.py
@@ -0,0 +1,39 @@
+"""
+整数数组 nums 按升序排列，数组中的值 互不相同 。
+在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，
+使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。
+例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为 [4,5,6,7,0,1,2] 。
+给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回 -1。
+
+链接：https://leetcode-cn.com/problems/search-in-rotated-sorted-array
+"""
+
+
+class Solution:
+    def search(self, nums, target):
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[left] <= nums[mid]:
+                if nums[left] <= target <= nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] <= target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return -1
+
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    nums = [3, 1]
+    target = 1
+    print(solution.search(nums, target))
